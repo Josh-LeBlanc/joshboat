8d2f0e1a75ab0781882c369968ce7302307c4bc8
Josh LeBlanc <joshcapitals07@gmail.com>
2025-03-02 14:11:40 +0100

    we now have pause, resume and stop functionality

--- joshboat.py
+++ joshboat.py
@@ -4,6 +4,7 @@ import asyncio
 import yt_dlp
 from dotenv import load_dotenv
 import collections
+import time
 
 
 def run_joshboat():
@@ -13,28 +14,31 @@ def run_joshboat():
     intents.message_content = True
     client = discord.Client(intents=intents)
 
-    prefix = "hey bitch "
-    vcs = {}
+    prefix = "?"
     queue = collections.deque()
+    voice_clients = []
 
     def play_queue(voice_client, ffmpeg_options):
         while len(queue) != 0:
+            time.sleep(2)
             if not voice_client.is_playing():
                 voice_client.play(discord.FFmpegPCMAudio(queue.pop(), **ffmpeg_options))
+                time.sleep(3)
 
     @client.event
     async def on_ready():
-        print(f"{client.user} is now playing music")
+        print(f"{client.user} is ready")
 
     @client.event
     async def on_message(message: discord.message.Message):
+
         if message.content.startswith(prefix + "play"):
             try:
-                if vcs == {}:
+                if len(voice_clients) == 0:
                     voice_client = await message.author.voice.channel.connect()
-                    vcs[voice_client.guild.id] = voice_client
+                    voice_clients.append(voice_client)
                 else:
-                    voice_client = list(vcs.items())[0][1]
+                    voice_client = voice_clients[0]
 
                 url = message.content.split()[-1]
 
@@ -56,6 +60,44 @@ def run_joshboat():
                 
                 asyncio.get_event_loop().run_in_executor(None, lambda: play_queue(voice_client, ffmpeg_options))
 
+            except Exception as e:
+                print(e)
+
+        elif message.content.startswith(prefix + "pause"):
+            try:
+                if len(voice_clients) == 0:
+                    voice_client = await message.author.voice.channel.connect()
+                    voice_clients.append(voice_client)
+                else:
+                    voice_client = voice_clients[0]
+
+                voice_client.pause()
+            except Exception as e:
+                print(e)
+
+        elif message.content.startswith(prefix + "resume"):
+            try:
+                if len(voice_clients) == 0:
+                    voice_client = await message.author.voice.channel.connect()
+                    voice_clients.append(voice_client)
+                else:
+                    voice_client = voice_clients[0]
+
+                voice_client.resume()
+            except Exception as e:
+                print(e)
+
+        elif message.content.startswith(prefix + "stop"):
+            try:
+                if len(voice_clients) == 0:
+                    voice_client = await message.author.voice.channel.connect()
+                    voice_clients.append(voice_client)
+                else:
+                    voice_client = voice_clients[0]
+
+                voice_client.stop()
+                await voice_client.disconnect()
+                voice_clients.remove(voice_client)
             except Exception as e:
                 print(e)
 
